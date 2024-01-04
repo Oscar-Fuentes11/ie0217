4c6cc1d49446cd5d150f6cdf708dcc5d341cb688
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-04 01:36:39 -0600

    2do commit

---
+++ Trabajos-Previos/TrabajoPrevio1/Sentencias.cpp
@@ -0,0 +1,17 @@
+    // Este programa verifica si el número es positivo.
+    // Si el número es mayor que 0, imprime "Condition is true"
+
+#include <iostream>
+
+int main() {
+    int number = 5;
+
+    if (number > 0) {
+        std::cout << "Condition is true" << std::endl;
+        // Agrega el código que deseas ejecutar cuando la condición es verdadera
+    }
+
+    // Código después del if
+
+    return 0;
+}
