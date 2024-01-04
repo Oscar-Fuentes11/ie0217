b2278cb0f421a74f4cb7036782cede3d921affe7
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-04 12:35:50 -0600

    2do commit

---
+++ Trabajos-Previos/TrabajoPrevio1/if-condition-false.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // Este programa verifica si el número es negativo.
+    // Si el número es menor que 0, imprime "Condition is false".
+
+    int number = 5;
+
+    if (number < 0) {
+        std::cout << "Condition is false" << std::endl;
+        // Agrega el código que deseas ejecutar cuando la condición es falsa
+    }
+
+    // Código después del if
+
+    return 0;
+}
