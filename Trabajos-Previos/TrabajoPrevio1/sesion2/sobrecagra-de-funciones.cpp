e27bdaae2e490d504569a51b4f26c44ed7e357a6
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-04 15:16:37 -0600

    2do commit

---
+++ Trabajos-Previos/TrabajoPrevio1/sesion2/sobrecagra-de-funciones.cpp
@@ -0,0 +1,29 @@
+// Este programa calcula el valor absoluto, funcionando para números enteros y flotantes.
+
+#include <iostream>
+
+using namespace std;
+
+// Función con parámetro de tipo float
+float absolute(float var) {
+    if (var < 0.0)
+        var = -var;
+    return var;
+}
+
+// Función con parámetro de tipo int
+int absolute(int var) {
+    if (var < 0)
+        var = -var;
+    return var;
+}
+
+int main() {
+    // Llamada a la función con parámetro int
+    cout << "Valor absoluto de -5 = " << absolute(-5) << endl;
+
+    // Llamada a la función con parámetro float
+    cout << "Valor absoluto de 5.5 = " << absolute(5.5f) << endl;
+
+    return 0;
+}
