d8bdf3808fdac90e0e0e715b64677cfd91afd6db
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-04 01:13:17 -0600

    1er commit

---
+++ Trabajos-Previos/TrabajoPrevio1/variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std;
+
+int main () {
+
+// Local variable:
+
+int a, b;
+
+int c;
+
+// initialization
+a= 10;
+b= 20;
+
+c = a+b;
+
+cout << c;
+
+return 0;
+}
