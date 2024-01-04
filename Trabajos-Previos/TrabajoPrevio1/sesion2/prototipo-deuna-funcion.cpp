6ebc872ba1ba078d136d673d2d1a58ff6abeb288
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-04 15:10:47 -0600

    2do commit

---
+++ Trabajos-Previos/TrabajoPrevio1/sesion2/prototipo-deuna-funcion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+// function prototype
+int add(int a, int b); 
+
+int main() {
+    int sum; 
+    // calling the function and storing
+    // the returned value in sum
+    sum = add(100, 78);
+
+    cout << "100 + 78 = " << sum << endl;
+
+    return 0;
+}
+
+// function definition
+int add(int a, int b) {
+    return (a + b);
+}
