a85bb12db3f754a2da194c616f36f8fe968e2889
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-05 18:35:46 -0600

    Labo1

--- Laboratorios/Laboratorio1/funciones.cpp
+++ Laboratorios/Laboratorio1/funciones.cpp
@@ -1,77 +1,80 @@
 #include "funciones.hpp"
-#include <string>
+#include <iostream>
 
-void mostrarMenu(){
+void mostrarMenu() {
     std::cout << "\n---Menu---\n*";
     std::cout << "1. Agregar empleados\n*";
     std::cout << "2. Listar empleados\n*";
     std::cout << "3. Eliminar empleados\n*";
     std::cout << "4. Salir\n*";
 }
-void procesarOpcion(Empleado empleados[],int& numEmpleados){
+
+void procesarOpcion(Empleado empleados[], int& numEmpleados) {
     int opcion;
-    std::cout <<"Ingrese una opcion:";
+    std::cout << "Ingrese una opcion:";
     std::cin >> opcion;
 
-    switch (opcion){
-      case 1: //Agregar empleado
-        agregarEmpleado(empleados, numEmpleados);
-        break;
-      case 2: // Lista de empleados
-        listarEmpleados (empleados, numEmpleados);
-        break;
-      case 3: //Eliminar empleado
-        eliminarEmpleado(empleados, numEmpleados);
-        break;
-      case 4: //Salir
-        std:: cout << "Saliendo del programa...\n*";
-        exit(0);
-      default;
-        std:: cout << "Opcion no valida. Intente de nuevo...\n*";
-   }
+    switch (opcion) {
+        case 1: // Agregar empleado
+            agregarEmpleado(empleados, numEmpleados);
+            break;
+        case 2: // Lista de empleados
+            listarEmpleados(empleados, numEmpleados);
+            break;
+        case 3: // Eliminar empleado
+            eliminarEmpleado(empleados, numEmpleados);
+            break;
+        case 4: // Salir
+            std::cout << "Saliendo del programa...\n*";
+            exit(0);
+            break;
+        default: 
+            std::cout << "Opcion no valida. Intente de nuevo...\n*";
+            break;
+    }
 }
 
-void agregarEmpleado (Empleado empleados[], int& numEmpleados){
-    if (numEmpleados < MAX_EMPLEADOS){
-    Empleado nuevoEmpleado;
-    nuevoEmpleado.id = numEmpleados + 1;
+void agregarEmpleado(Empleado empleados[], int& numEmpleados) {
+    if (numEmpleados < MAX_EMPLEADOS) {
+        Empleado nuevoEmpleado;
+        nuevoEmpleado.id = numEmpleados + 1;
 
-    std::cout << "Ingrese el nombre del empleado: ";
-    std::cin >> nuevoEmpleado_nombre;
+        std::cout << "Ingrese el nombre del empleado: ";
+        std::cin >> nuevoEmpleado.nombre;
 
-    std::cout << "Ingrese el salario del empleado: ";
-    std::cin >> nuevoEmpleado_salario;
+        std::cout << "Ingrese el salario del empleado: ";
+        std::cin >> nuevoEmpleado.salario;
 
-    empleados[numEmpleados++]= nuevoEmpleado;
-    std::cout << "Empleado agregado con exito\n*";
+        empleados[numEmpleados++] = nuevoEmpleado;
+        std::cout << "Empleado agregado con exito\n*";
     } else {
-      std ::cout << "No se puede agregar mas, limite alcanzado.\n"
-      }
-  }
-  
-  void listarEmpleados (const Empleado empleados[], int& numEmpleados){
-    std :: cout << "lista de empleados \n";
-    
-    for (int i = 0; i < numEmpleados [], int& numEmpleados) {
-        std::cout << "ID:" << empleados [i]. id << "Nombre:" <<
-        empleados [i].nombre << "salario:" << empleados [i]. salario << "\n";
-  }
- }
+        std::cout << "No se puede agregar mas, limite alcanzado.\n"; 
+    }
+}
 
- void eliminarEmpleado (Empleado empleados[], int& numEmpleados){
+void listarEmpleados(const Empleado empleados[], int& numEmpleados) {
+    std::cout << "lista de empleados \n";
+
+    for (int i = 0; i < numEmpleados; ++i) { 
+        std::cout << "ID:" << empleados[i].id << " Nombre:" << 
+        empleados[i].nombre << " salario:" << empleados[i].salario << "\n";
+    }
+}
+
+void eliminarEmpleado(Empleado empleados[], int& numEmpleados) {
     int idEliminar;
 
-    std :: cout << "Ingrese el ID a eliminar:";
-    std :: cin >> idEliminar;
+    std::cout << "Ingrese el ID a eliminar:";
+    std::cin >> idEliminar;
 
-    for (int i = 0; i < numEmpleados; ++i){
-        if (empleados[i].id ==idEliminar) {
-            for (int j = i; j < numEmpleados - 1; ++j){
-                empleados [j] = empleados [j+1];
+    for (int i = 0; i < numEmpleados; ++i) {
+        if (empleados[i].id == idEliminar) {
+            for (int j = i; j < numEmpleados - 1; ++j) {
+                empleados[j] = empleados[j + 1];
             }
             --numEmpleados;
-            std :: cout << "Empleado eliminado con exito. \n";
+            std::cout << "Empleado eliminado con exito. \n";
             return;
         }
     }
- }
+}   //ya esta parte hasta aca llega
