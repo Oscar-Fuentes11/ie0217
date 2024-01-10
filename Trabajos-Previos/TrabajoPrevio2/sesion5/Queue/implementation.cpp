007cd92ad2e821a0903261eb5bbc35a2b0b3fa27
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-10 02:05:18 -0600

    Agregando la implementación en c++

---
+++ Trabajos-Previos/TrabajoPrevio2/sesion5/Queue/implementation.cpp
@@ -0,0 +1,31 @@
+// Queue implementation in C++
+
+#include <iostream>
+
+#define SIZE 5
+
+using namespace std;
+
+class Queue {
+private:
+    int items[SIZE], front, rear;
+public:
+    Queue() {
+        front = -1;
+        rear = -1;
+    }
+
+    bool isFull() {
+        if (front == 0 && rear == SIZE - 1) {
+            return true;
+        }
+        return false;
+    }
+
+    bool isEmpty() {
+        if (front == -1)
+            return true;
+        else
+            return false;
+    }
+};
