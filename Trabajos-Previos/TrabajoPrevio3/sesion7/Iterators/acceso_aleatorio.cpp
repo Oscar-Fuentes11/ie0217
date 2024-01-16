70149bf6a54bfe60e04a6ebc733be6d440c87ef9
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-16 02:28:27 -0600

    Agregando ejemplo del iterador de acceso aleatorio

---
+++ Trabajos-Previos/TrabajoPrevio3/sesion7/Iterators/acceso_aleatorio.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // Crear un vector
+    vector<int> vec{1, 2, 3, 4};
+
+    // Crear iteradores para apuntar al primer y al último elemento
+    vector<int>::iterator itr_first = vec.begin();
+    vector<int>::iterator itr_last = vec.end() - 1;
+
+    // Mostrar los elementos del vector
+    cout << "First Element: " << *itr_first << endl;
+    cout << "Second Element: " << itr_first[1] << endl;
+    cout << "Second Last Element: " << *(itr_last - 1) << endl;
+    cout << "Last Element: " << *itr_last << endl;
+
+    return 0;
+}
