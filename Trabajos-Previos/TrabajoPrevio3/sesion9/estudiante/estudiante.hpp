daa9d611cb8d32dd79bb6ba3a973221138a1d3af
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-17 18:45:38 -0600

    Agregando codes de estudiante, con su respectivo Makefile

---
+++ Trabajos-Previos/TrabajoPrevio3/sesion9/estudiante/estudiante.hpp
@@ -0,0 +1,18 @@
+#ifndef ESTUDIANTE_HPP
+#define ESTUDIANTE_HPP
+
+#include <string>
+
+
+class Estudiante {
+    public:
+    Estudiante(const std::string& nombre, int edad);
+    void mostrarDatos();
+    
+    private:
+    std::string nombre;
+    int edad;
+    int edad2;
+};
+
+#endif
