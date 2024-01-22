8871e387518d2ea80d66180ef9c5bcc2be5c5376
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-22 13:25:36 -0600

    Agregando codes de salidas en python

---
+++ Trabajos-Previos/TrabajoPrevio4/sesion10/SalidasPython.py
@@ -0,0 +1,34 @@
+# primer ejemplo
+
+print("Este es el primer ejemplo:")
+number= -10.6
+name="IE0217"
+
+#print literals
+print(5)
+
+#print variables
+print(number)
+print(name)
+
+#join strings
+print('Este curso es' + name)
+
+#Segundo Ejemplo
+print("Este es el segundo ejemplo:")
+print('New Year',2023, 'see you soon!', sep= '. ')
+
+
+#Tercer ejemplo
+print("Este es el tercer ejemplo:")
+#print with end whitespace
+print('Good Morning!', end= ' ')
+print ('It is rainy today')
+
+
+#Cuarto ejemplo
+print("Este es el cuarto ejemplo:")
+x= 5
+y=10
+
+print('The value of x is {} and y is {}' .format(x,y))
