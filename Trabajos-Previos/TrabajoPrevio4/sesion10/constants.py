1ecead7c3a8fc43532002ca75a6f6d4aa3cf2eb9
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-22 11:24:33 -0600

    Agregando code de una constante.py

---
+++ Trabajos-Previos/TrabajoPrevio4/sesion10/constants.py
@@ -0,0 +1,13 @@
+#Declare constants
+PI=3.14
+GRAVITY=9.8
+
+#import constant file we created above
+import constants
+
+print (constants.PI)
+print(constants.GRAVITY)
+
+
+#Nota importante, no hay constantes en python, nombrarlos en mayusculas es una convencion para 
+#separarlos de las variables, sin embargo, en realidad no evita la reasignacion.
