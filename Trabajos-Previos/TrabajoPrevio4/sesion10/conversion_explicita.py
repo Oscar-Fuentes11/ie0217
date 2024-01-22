62d3119c3c4df99b5625671ff4df282ddc6dcb3f
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-22 13:04:59 -0600

    Agregando tipo de conversion explicita

---
+++ Trabajos-Previos/TrabajoPrevio4/sesion10/conversion_explicita.py
@@ -0,0 +1,13 @@
+num_string = '12'
+num_integer = 23
+
+print("Data type of num_string before type Casting:", type(num_string))
+
+#explicit type conversion
+num_string = int(num_string)
+
+print("Data type of num_string after type Casting:", type(num_string))
+num_sum=num_integer+num_string
+
+print("Sum:", num_sum)
+print("Data type of num_sum",type(num_sum))
