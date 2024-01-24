510e623217cbf5a67c7fce9a11834c5ef576d3a0
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-23 22:14:13 -0600

    Se agrego ejemplo llamando argumentos

---
+++ Trabajos-Previos/TrabajoPrevio4/sesion11/Funciones/arguments.py
@@ -0,0 +1,8 @@
+def sumar (valor1, valor2):
+    print ("La suma de", valor1, "con", valor2, "es:", valor1 + valor2)
+    suma = valor1 + valor2
+    return suma
+
+sumar(15, 14)
+sumar(15, 15)
+sumar(153, 244)
