91444c0c0bedee03abcb4ef46c9127b18a6891c9
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-23 20:47:00 -0600

    se agrega ejemplo de if_elif_else condition

---
+++ Trabajos-Previos/TrabajoPrevio4/sesion11/control_de_flujos/if_elif_else.py
@@ -0,0 +1,10 @@
+number = 10
+
+if number > 0:
+    print ('positive number.')
+elif number ==0:
+    print ('zero')
+else:
+    print ('Negative number')
+
+    print ('This statement is always executed')
