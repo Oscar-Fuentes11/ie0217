f233a6c52d84a1fc6455e3c661f800c6c780d739
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-24 14:39:08 -0600

    Agregando code sobe el membership.

---
+++ Trabajos-Previos/TrabajoPrevio4/sesion11/Tipos_De_Datos/membershipTest_Dict.py
@@ -0,0 +1,9 @@
+# Membership test for Dictionary keys
+squares = {1: 1, 3: 9, 5: 25, 7: 49, 9: 81}
+
+#Output: true
+print (1 in squares)# prints true 
+print (2 not in squares)# prints true 
+
+#membership tests for key only not value
+print(49 in squares)# prints false
