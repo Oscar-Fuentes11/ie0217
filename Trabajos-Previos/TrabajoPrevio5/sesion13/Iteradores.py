acd4ea2cba3c73b92459df9171cdf903ec9020d2
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-29 17:07:36 -0600

    Agregando ejemplo de Iteradores.

---
+++ Trabajos-Previos/TrabajoPrevio5/sesion13/Iteradores.py
@@ -0,0 +1,16 @@
+#define a list
+my_list = [4, 7, 0]
+
+#create an iterator from the list
+iterator = iter(my_list)
+
+#get the first element of the iterator
+print (next(iterator)) #acá se imprime el 4
+
+#get the second element of the iterator
+print (next(iterator)) #acá se imprime el 7
+
+#get the third element of the iterator
+print (next(iterator)) #acá se imprime el 0
+
+
