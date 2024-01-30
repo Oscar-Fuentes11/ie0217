eb3f732e6245eaaeafe4e205eac5bfe72fa8336e
Oscar-Fuentes11 <oscar.fuentescordoba@ucr.ac.cr>
2024-01-30 02:41:26 -0600

    Agregando ejemplo de graficos de linea.

--- Trabajos-Previos/TrabajoPrevio5/sesion15/GraficosDeLinea.py
+++ Trabajos-Previos/TrabajoPrevio5/sesion15/GraficosDeLinea.py
@@ -2,15 +2,15 @@
 import matplotlib.pyplot as plt
 
 # Datos de ejemplo para el eje X y Y
-x = [1, 2, 3, 4, 5]  # Valores del eje X
-y = [10, 12, 5, 8, 14]  # Valores del eje Y
+x = [1, 2, 3, 4, 5]  
+y = [10, 12, 5, 8, 14]  
 
 # Creación de un gráfico de línea simple
 plt.plot(x, y)
 
 # Personalización del gráfico para mejorar su visualización
-plt.xlabel('Eje X')  # Etiqueta para el eje X
-plt.ylabel('Eje Y')  # Etiqueta para el eje Y
+plt.xlabel('Eje X')  
+plt.ylabel('Eje Y')  
 plt.title('Gráfico de Línea Simple')  # Título del gráfico
 plt.grid(True)  # Añade una cuadrícula al gráfico para facilitar la lectura
 
